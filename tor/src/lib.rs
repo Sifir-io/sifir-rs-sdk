d1bbe51e961f47bc590dd3100d32d24329088445
Gus <gabidi@gmail.com>
2020-11-12 10:53:50 -0500

    Java glue works but back to closure shit :/

--- tor/src/lib.rs
+++ tor/src/lib.rs
@@ -1,22 +1,22 @@
 use anyhow::Result;
+use futures::Future;
+use lazy_static::*;
 use libtor::{HiddenServiceVersion, Tor, TorAddress, TorFlag};
 use serde::{Deserialize, Serialize};
 #[cfg(test)]
 use serial_test::serial;
+use std::cell::RefCell;
 use std::fs;
 use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
-use tokio::macros::support::Future;
 use tokio::net::TcpStream;
 use torut::control::{AsyncEvent, AuthenticatedConn, ConnError, UnauthenticatedConn};
-use lazy_static::*;
-use std::sync::{Mutex,Arc};
-use std::cell::RefCell;
 
-type E = Pin<Box<dyn Future<Output = Result<bool, ()>>>>;
-type F = Box<dyn Fn(AsyncEvent<'static>) -> E>;
-type G = AuthenticatedConn<TcpStream,F>;
-
+type G = AuthenticatedConn<
+    TcpStream,
+    Box<dyn Fn(AsyncEvent<'static>) -> Pin<Box<dyn Future<Output = Result<(), ConnError>> + '_>>>,
+>;
 lazy_static! {
      //runtime with threaded pool
      static ref RUNTIME: Mutex<tokio::runtime::Runtime> = Mutex::new(tokio::runtime::Builder::new()
@@ -24,7 +24,7 @@ lazy_static! {
          .enable_all()
          .build()
          .unwrap());
-     static ref CTL_CONN: Arc<Mutex<Option<G>>> = Arc::new(Mutex::new(None));
+     // static ref CTL_CONN: Arc<Mutex<Option<G>>> = Arc::new(Mutex::new(None));
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -38,12 +38,12 @@ pub struct TorService {
     socks_port: u16,
     control_port: String,
     _handle: Option<JoinHandle<Result<u8, libtor::Error>>>,
-    // _ctl: RefCell<Option<AuthenticatedConn<TcpStream,F>>>
+    _ctl: RefCell<Option<G>>,
 }
 
 trait TorServiceStatus {
     // async fns in traits are a shitshow
-    fn wait_bootstrap( &mut self) -> Pin<Box<dyn Future<Output = Result<bool, ()>> + '_>>;
+    fn wait_bootstrap(&mut self) -> Pin<Box<dyn Future<Output = Result<bool, ()>> + '_>>;
     fn shutdown(self);
 }
 impl From<TorServiceParam> for TorService {
@@ -92,9 +92,15 @@ impl From<TorServiceParam> for TorService {
             socks_port,
             control_port,
             _handle: Some(handle),
-            // _ctl: RefCell::new(None),
+            _ctl: RefCell::new(None),
         }
     }
+}
+
+fn handler(event: AsyncEvent<'static>) -> Pin<Box<dyn Future<Output=Result<(),ConnError>> + '_>> {
+    Box::pin(async move {
+        Ok(())
+    })
 }
 impl TorService {
     pub fn new(param: TorServiceParam) -> Self {
@@ -108,25 +114,23 @@ impl TorService {
         .unwrap();
         reqwest::Client::builder().proxy(proxy).build()
     }
-	pub fn take_control(&self){
-		(*RUNTIME).lock().unwrap().block_on(async {
-				let mut ac = self.get_control_auth_conn(Some(|event| async move 
-				{
-                	println!("{:#?}", event);
-                	Ok(())
-            	})).await;
-                ac.wait_bootstrap().await.unwrap();
-                let mut conn = (*CTL_CONN).lock().unwrap();
-                 *conn = Some(ac);
-                // STORE THIS!
-                // *self._ctl.borrow_mut() = Some(ac);dd
-		});
+    pub fn take_control(&self) {
+        (*RUNTIME).lock().unwrap().block_on(async {
+            let mut ac = self
+                .get_control_auth_conn(Some(Box::new(handler)))
+                .await;
+            ac.wait_bootstrap().await.unwrap();
+            // let mut conn = (*CTL_CONN).lock().unwrap().unwrap();
+            //  *conn = Some(ac);
+            // STORE THIS!
+             *self._ctl.borrow_mut() = Some(ac);
+        });
     }
-    async fn get_control_auth_conn<H,F>(&self, handle: Option<H>) -> AuthenticatedConn<TcpStream, H> 
-where
-    H: Fn(AsyncEvent<'static>) -> F,
-    F: Future<Output = Result<(), ConnError>>,
-{
+    async fn get_control_auth_conn<G>(
+        &self,
+        handle: Option<G>,
+    ) -> AuthenticatedConn<TcpStream, G>
+    {
         let s = TcpStream::connect(self.control_port.trim()).await.unwrap();
         let mut utc = UnauthenticatedConn::new(s);
         // returns node info + cookie location ?
@@ -138,7 +142,7 @@ where
         let mut ac = utc.into_authenticated().await;
         if handle.is_some() {
             ac.set_async_event_handler(handle);
-        } 
+        }
         ac
     }
 }
@@ -148,9 +152,7 @@ where
     H: Fn(AsyncEvent<'static>) -> F,
     F: Future<Output = Result<(), ConnError>>,
 {
-    fn wait_bootstrap(
-        &mut self,
-    ) -> Pin<Box<dyn Future<Output = Result<bool, ()>> + '_>> {
+    fn wait_bootstrap(&mut self) -> Pin<Box<dyn Future<Output = Result<bool, ()>> + '_>> {
         // Wait for boostrap to be done
         Box::pin(async move {
             let mut input = String::new();
@@ -183,7 +185,7 @@ mod tests {
         assert_eq!(service.control_port.contains("127.0.0.1:"), true);
         assert_eq!(service._handle.is_some(), true);
         let mut control_conn = service
-            .get_control_auth_conn(Some(|_: AsyncEvent<'static>| async move { Ok(()) }))
+            .get_control_auth_conn(Some(handler))
             .await;
         let _ = control_conn.wait_bootstrap().await;
         control_conn.take_ownership().await;
@@ -191,24 +193,24 @@ mod tests {
         let _ = service._handle.unwrap().join();
     }
 
-    #[tokio::test]
-    #[serial(tor)]
-    async fn should_get_onion_url() {
-        let service: TorService = TorServiceParam {
-            port: 8000,
-            socks_port: Some(19054),
-            data_dir: String::from("/tmp/torlib3"),
-        }
-        .into();
-        let mut control_conn = service.take_control();
-        let client = service.get_client().unwrap();
-        let resp = client
-            .get("http://keybase5wmilwokqirssclfnsqrjdsi7jdir5wy7y7iu3tanwmtp6oid.onion")
-            .send()
-            .await
-            .unwrap();
-        assert_eq!(resp.status(), 200);
-        control_conn.shutdown();
-        let _ = service._handle.unwrap().join();
-    }
+    //#[tokio::test]
+    //#[serial(tor)]
+    //async fn should_get_onion_url() {
+    //    let service: TorService = TorServiceParam {
+    //        port: 8000,
+    //        socks_port: Some(19054),
+    //        data_dir: String::from("/tmp/torlib3"),
+    //    }
+    //    .into();
+    //    let mut control_conn = service.take_control();
+    //    let client = service.get_client().unwrap();
+    //    let resp = client
+    //        .get("http://keybase5wmilwokqirssclfnsqrjdsi7jdir5wy7y7iu3tanwmtp6oid.onion")
+    //        .send()
+    //        .await
+    //        .unwrap();
+    //    assert_eq!(resp.status(), 200);
+    //    control_conn.shutdown();
+    //    let _ = service._handle.unwrap().join();
+    //}
 }
