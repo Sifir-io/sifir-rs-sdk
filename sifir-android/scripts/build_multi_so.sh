ef6de5f0d37528ce74addbd3dc2178998d7dec00
g-homebase <gabidi@gmail.com>
2021-01-21 06:35:53 -0500

    fix: [android 11] target api 30

--- sifir-android/scripts/build_multi_so.sh
+++ sifir-android/scripts/build_multi_so.sh
@@ -21,16 +21,16 @@ retVal=$?
 [ ! $retVal -eq 0 ] && exit 1;
 fi
 
-cargo ndk --android-platform 26 --target x86_64-linux-android -- build -p sifir-android --release
+cargo ndk --android-platform 30 --target x86_64-linux-android -- build -p sifir-android --release
 retVal=$?
 [ ! $retVal -eq 0 ] && exit 1;
-cargo ndk --android-platform 26 --target aarch64-linux-android -- build -p sifir-android --release
+cargo ndk --android-platform 30 --target aarch64-linux-android -- build -p sifir-android --release
 retVal=$?
 [ ! $retVal -eq 0 ] && exit 1;
-cargo ndk --android-platform 26 --target armv7-linux-androideabi -- build -p sifir-android --release
+cargo ndk --android-platform 30 --target armv7-linux-androideabi -- build -p sifir-android --release
 retVal=$?
 [ ! $retVal -eq 0 ] && exit 1;
-cargo ndk --android-platform 26 --target i686-linux-android -- build -p sifir-android --release
+cargo ndk --android-platform 30 --target i686-linux-android -- build -p sifir-android --release
 retVal=$?
 [ ! $retVal -eq 0 ] && exit 1;
 
