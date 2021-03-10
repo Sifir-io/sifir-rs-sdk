74b49a9658097e1a405a725f90d5859e38e15e52
Gus <gabidi@gmail.com>
2021-03-10 14:32:59 -0500

    feat: [ios bindgen] Conditional header based on features + bdk copy script

--- sifir-ios/build.rs
+++ sifir-ios/build.rs
@@ -4,10 +4,12 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let outputname =
         env::var("SIFIR_CBINDGEN_OUTPUT_FILENAME").unwrap_or(String::from("sifir-tor.h"));
+
     cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(C)
-        .with_include("sifir_typedef.h")
+        .with_define("feature","tor_daemon", "TOR_DAEMON")
+        .with_define("feature","btc_wallet", "BTC_WALLET")
         .generate()
         .expect("Unable to generate bindings!!")
         .write_to_file(format!("./output/{}", outputname));
